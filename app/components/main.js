20bca9903ea4708c1b5276103ca5eea8bf67e766
jessephilip <jessephilip@gmail.com>
2017-01-18 00:13:53 -0600

    Loading initial files. Not working at the moment.

---
+++ app/components/main.js
@@ -0,0 +1,22 @@
+// Include React
+import React from "react";
+
+// Creating the Main component
+export default class Main extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    // Here we render the function
+    render() {
+        return (
+
+            <div>
+                <h1>Hello World!</h1>
+
+            </div>
+        );
+    }
+}
+
+// Export the component back for use in other files
